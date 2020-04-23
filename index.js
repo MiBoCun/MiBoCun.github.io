bfa308329c020918c37137824e9978eeff107e29
MiBoCun <Midadada@aliyun.com>
2020-04-23 21:38:09 +0800

    add chinese and english

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-let introduceText="Hello, I'm a front-end development engineer from Shenzhen, China. I like sports, and I'm good at running and billiards. MiBoCun is my former name. If we are lucky to know each other and become friends, you will know my real name. In my spare time, I will not update my GitHub constantly, and browse the latest topics at the same time. Welcome to pay attention to each other. If I receive your attention, I will browse your personal blog as soon as possible. Recently, I've been focusing on vue.js &amp; Vue router &amp; vuex, as well as front-end component development ideas. The above English introduction is my use of Baidu translation software translation, if there is a wrong understanding, welcome to correct. By the way, now the new coronavirus is spreading all over the world. I hope you are safe and sound in the distance and continue to have ideals.";
+let introduceEnglish="Hello, I'm a front-end development engineer from Shenzhen, China. I like sports, and I'm good at running and billiards. MiBoCun is my former name. If we are lucky to know each other and become friends, you will know my real name. In my spare time, I will not update my GitHub constantly, and browse the latest topics at the same time. Welcome to pay attention to each other. If I receive your attention, I will browse your personal blog as soon as possible. Recently, I've been focusing on vue.js &amp; Vue router &amp; vuex, as well as front-end component development ideas. The above English introduction is my use of Baidu translation software translation, if there is a wrong understanding, welcome to correct. By the way, now the new coronavirus is spreading all over the world. I hope you are safe and sound in the distance and continue to have ideals.";
+let introduceChinese="你好 我是一名来自中国深圳的前端开发工程师.我喜欢运动，擅长跑步，桌球。米柏存是我的曾用名,如果我们有幸认识成为朋友，那么你会知道我的真实名字。业余时间我不会不断更新我的gitHub,同时浏览最新的话题。欢迎互相关注,如果收到你的关注，我会第一时间浏览你的个人博客。最近我在持续关注vue.js&vue-router&vuex，以及前端组件化开发思想。 以上这段英文介绍是我使用百度翻译软件翻译，如果有理解不对，欢迎指正。对了，现在新冠病毒全球蔓延，希望远方的你安然无恙，继续心怀理想。";
 
 window.onload=function(){
     let textPartDom= document.getElementById('textPart');
-    console.log(typeof a);
+    let texTitleDom=document.getElementById("texTitle");
+
     let i = 0;
     timer=setInterval(function(){
-        textPartDom.innerHTML=introduceText.substring(0,i)
+        textPartDom.innerText=introduceEnglish.substring(0,i)
         i++;
-        if(textPartDom.innerHTML==introduceText){clearInterval(timer);};
-    },50);
+        if(textPartDom.innerText==introduceEnglish){
+            clearInterval(timer);
+
+            $("#langBox").animate({marginTop:'0px'},800)
+
+
+    }
+    },50)
+    let languageDoms=document.getElementsByClassName("languageIcon");
+    for(let k=0;k<languageDoms.length;k++){
+        languageDoms[k].onclick=function (e) {
+            let eDom=e.target;
+
+            let language=eDom.alt;
+            if(language=="English"){
+                textPartDom.innerText=introduceEnglish;
+                texTitleDom.innerText="Welcome to my github blog";
+            }else{
+                textPartDom.innerText=introduceChinese;
+                texTitleDom.innerText="欢迎来到我的github博客";
+
+            }
+        }
+
+    }
+
+
+
+
+
 }
