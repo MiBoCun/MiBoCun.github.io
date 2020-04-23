66f7416dc11b4d6d6dd002485bfe20903c5bc6af
MiBoCun <Midadada@aliyun.com>
2020-04-23 22:27:15 +0800

    optimize ui

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 let introduceEnglish="Hello, I'm a front-end development engineer from Shenzhen, China. I like sports, and I'm good at running and billiards. MiBoCun is my former name. If we are lucky to know each other and become friends, you will know my real name. In my spare time, I will not update my GitHub constantly, and browse the latest topics at the same time. Welcome to pay attention to each other. If I receive your attention, I will browse your personal blog as soon as possible. Recently, I've been focusing on vue.js &amp; Vue router &amp; vuex, as well as front-end component development ideas. The above English introduction is my use of Baidu translation software translation, if there is a wrong understanding, welcome to correct. By the way, the new coronavirus is now spreading all over the world. I hope you are safe and sound in the distance and continue to have ideals.";
-let introduceChinese="你好 我是一名来自中国深圳的前端开发工程师.我喜欢运动，擅长跑步，桌球。米柏存是我的曾用名,如果我们有幸认识成为朋友，那么你会知道我的真实名字。业余时间我不会不断更新我的gitHub,同时浏览最新的话题。欢迎互相关注,如果收到你的关注，我会第一时间浏览你的个人博客。最近我在持续关注vue.js&vue-router&vuex，以及前端组件化开发思想。 以上这段英文介绍是我使用百度翻译软件翻译，如果有理解不对，欢迎指正。对了，新冠病毒现在全球蔓延，希望远方的你安然无恙，继续心怀理想。";
+let introduceChinese="你好 我是一名来自中国深圳的前端开发工程师.我喜欢运动，擅长跑步和桌球。米柏存是我的曾用名,如果我们有幸认识成为朋友，那么你会知道我的真实名字。业余时间我不会不断更新我的gitHub,同时浏览最新的话题。欢迎互相关注,如果收到你的关注，我会第一时间浏览你的个人博客。最近我在持续关注vue.js&vue-router&vuex，以及前端组件化开发思想。 以上这段英文介绍是我使用百度翻译软件翻译，如果有理解不对，欢迎指正。对了，新冠病毒现在全球蔓延，希望远方的你安然无恙，继续心怀理想。";
 
 window.onload=function(){
     let textPartDom= document.getElementById('textPart');
